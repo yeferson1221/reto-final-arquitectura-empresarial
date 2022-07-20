03988a33896487c5df45ce8c6ccd4bb8f10d258a
SantiagoOspinaO <santiago.m200@outlook.es>
2022-07-19 20:12:36 -0500

    Actualizacion al proyecto

--- Arquetipo-Bancolombia-Heroe/domain/model/src/main/java/co/com/sofka/model/game/Jugador.java
+++ Arquetipo-Bancolombia-Heroe/domain/model/src/main/java/co/com/sofka/model/game/Jugador.java
@@ -1,4 +1,12 @@
 package co.com.sofka.model.game;
 
 public class Jugador {
+
+    private String id;
+    private String turno;
+
+    public Jugador(String id, String turno) {
+        this.id = id;
+        this.turno = turno;
+    }
 }
